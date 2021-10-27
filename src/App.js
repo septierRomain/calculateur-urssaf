9a1749f4235db85d15faa31f82c227cb6de071fd
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2021-10-27 11:52:34 +0200

    integration OK

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Banner from './Component/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
